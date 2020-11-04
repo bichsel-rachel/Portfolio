df93e4495f21ba370a639049d7b5e92b5de5fed7
Rachel Bichsel <rachbichsel@gmail.com>
2020-11-04 12:25:26 -0700

    testing 2

--- index.js
+++ index.js
@@ -64,13 +64,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-//const PORT = process.env.PORT || 5000 || 18765;
-//app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+ const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku). See further comments below
 app.use(enforce.HTTPS());
  
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(PORT, function() {
+    console.log('Express server listening on port ' + PORT);
 });
